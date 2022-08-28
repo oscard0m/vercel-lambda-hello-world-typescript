5ee68ca6c05cf678dfa82703354e0548fccce334
Oscar Dominguez <dominguez.celada@gmail.com>
2022-08-28 16:22:43 +0200

    feat(serverless): move /api folder under src file

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { name } = request.query;
